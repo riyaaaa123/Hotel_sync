425e74dbd5f13dad02cc34f008eac0e3fbb6bb63
riyaaaa123 <riyajindal769@gmail.com>
2025-04-10 01:37:44 +0530

    [fix] deleted ml and modified frontend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,5 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
-    path("api/", include("prediction.urls")),
 ]
